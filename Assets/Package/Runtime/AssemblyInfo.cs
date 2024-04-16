43fc3db4a11de7b1838a4e645c77305bc7a275c5
Valdeci Danilo <valdecidanilo@live.com>
2024-04-16 16:15:12 -0300

    feat: added custom button scripts

---
+++ Assets/Package/Runtime/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Runtime.CompilerServices;
+
+#if UNITY_EDITOR
+[assembly: InternalsVisibleTo("Unity.TextCore.Editor")]
+[assembly: InternalsVisibleTo("Unity.TextMeshPro.Editor")]
+#endif
---
+++ Assets/Package/Runtime/CustomButtonClass.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace CustomButton
+{
+    [AddComponentMenu("UI/Custom Button", 31)]
+    public class CustomButtonClass : CustomButtonBase
+    {
+        public override void OnClick()
+        {
+            base.OnClick();
+        }
+    }
+}
